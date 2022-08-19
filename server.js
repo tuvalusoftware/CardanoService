e78f29e673bb6a3740b129255317779409473d01
Khang Tran Quoc <tqk@192.168.2.3>
2022-08-19 10:20:08 +0700

    Msg: add CORS

--- server.js
+++ server.js
@@ -8,7 +8,21 @@ import { routers } from "./routers";
 const app = express();
 const server = http.createServer(app);
 
-var whitelist = [
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+import swaggerUi from "swagger-ui-express";
+import { swaggerDocs } from "./api-docs";
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "Cardano Service",
+  customfavIcon: "https://fuixlabs.com/img/favicons/favicon.ico"
+}));
+
+import Logger from "./Logger";
+
+const whitelist = [
   "https://cardano-fuixlabs.ap.ngrok.io",
   // "https://docrender-fuixlabs.ap.ngrok.io",
   // "https://auth-fuixlabs.ap.ngrok.io",
@@ -21,29 +35,16 @@ var whitelist = [
 
 const corsOptions = {
   origin: function (origin, callback) {
+    console.log(origin);
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("NOT_ALLOWEB_BY_CORS"));
     }
   }
-} 
+};
 
 app.use(cors(corsOptions));
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-
-import swaggerUi from "swagger-ui-express";
-import { swaggerDocs } from "./api-docs";
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
-  customCss: '.swagger-ui .topbar { display: none }',
-  customSiteTitle: "Cardano Service",
-  customfavIcon: "https://fuixlabs.com/img/favicons/favicon.ico"
-}));
-
-import Logger from "./Logger";
 
 export const start = async (params) => {
   routers(app);
