66da9fac438bcdeb76209ed369ded4a688a16b10
Khang Tran Quoc <tqk@Khangs-MacBook-Pro.local>
2022-09-05 22:17:51 +0700

    Msg: Remove info

--- api-docs/basicInfo.js
+++ api-docs/basicInfo.js
@@ -4,10 +4,5 @@ export const basicInfo = {
     title: "Cardano service",
     description: "This is a simple API to interactive with NFT on the Cardano blockchain",
     version: "2.0.0",
-    contact: {
-      name: "Khang, Tran Quoc",
-      email: "tkhang@ferdon.io",
-      url: "https://fuixlabs.com",
-    },
   },
 };
